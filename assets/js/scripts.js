89f26f8023e564219d4a0409bcf92a831367beff
nancyhenry89 <nancyhenry89@gmail.com>
2018-08-12 17:45:12 +0200

    Step2

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -12,5 +12,14 @@ $(document).ready(function(){
                     $('.color').removeClass('selected');
                 $(this).addClass('selected');
                 $(this).parents('.colors').siblings('.pic').find('img').attr("src","assets/imgs/"+img+".png")
+                $('.selected-phone img').attr("src","assets/imgs/"+img+".png")
                 });
+        $('.pick-btn').click(function(){
+            $("#step1").slideUp();
+            $("#step2").slideDown();
+        });
+        $('#back').click(function(){
+            $("#step2").slideUp();
+            $("#step1").slideDown();
+        });
 });
